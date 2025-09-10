2ea814d93a954b764c3e10d3071358ad69b64669
Sanna Wiklund <142911012+sannawiklund@users.noreply.github.com>
2025-09-10 21:31:59 +0200

    feat: (About Us/CTA) Add About Us and CTA-segment. Draft 1

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import './index.css'
 import './App.css'
 import Hero from './Components/Hero'
+import AboutUs from './Components/AboutUs'
+import CTA from './Components/CTA'
 
 function App() {
   return (
     <>
       <Hero />
+      <CTA />
+      <AboutUs />
+
     </>
   )
 }
