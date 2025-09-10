036ef22bdadfbf0696277abfc166b50c85bd3885
Sanna Wiklund <142911012+sannawiklund@users.noreply.github.com>
2025-09-10 14:09:35 +0200

    feat: (Hero) Add initial Hero-draft

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import Logo from '../Assets/Alkion_logo.png';
+import Video from '../Assets/Example-video.mp4';
+import '../Styling/Hero.css';
+
+
+function Hero() {
+
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+    const scrollToSection = (id) => {
+        const section = document.getElementById(id);
+        if (section) {
+            section.scrollIntoView({ behavior: "smooth" });
+        }
+    };
+
+    return (
+        <div className="flex flex-col h-screen">
+
+            <video
+                className="absolute top-0 left-0 w-full h-full object-cover z-[-1] opacity-100"
+                src={Video}
+                autoPlay
+                loop
+                muted
+                playsInline
+            />
+
+            <nav className="bg-[#f4f2f2] py-1 px-8 flex justify-between items-center">
+                <img className="h-20 object-cover" src={Logo} />
+                <ul className="flex space-x-6">
+                    <li><a href="#" className="text-gray-800 font-semibold">Home</a></li>
+                    <li><a href="#" className="text-gray-800 font-semibold">About</a></li>
+                    <li><a href="#" className="text-gray-800 font-semibold">Product & Services</a></li>
+                    <li><a href="#" className="text-gray-800 font-semibold">History & Theory</a></li>
+                    <li><a href="#" className="text-gray-800 font-semibold">Contact</a></li>
+                </ul>
+            </nav>
+
+
+
+            <header className="flex-grow flex flex-col justify-center items-center px-8">
+                <h1 className="text-6xl font-bold text-white text-center mb-8" id='Title'>Welcome to Alkion Technologies</h1>
+                <p className="text-white text-xl text-center mb-5 font-medium ">THE FUTURE OF ALKALINITY MEASUREMENT</p>
+            </header>
+        </div>
+    );
+}
+
+export default Hero;
