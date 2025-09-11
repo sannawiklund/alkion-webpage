aee58e7247e887640fa4afa1536a814e36f5e5b3
Sanna Wiklund <142911012+sannawiklund@users.noreply.github.com>
2025-09-11 18:28:13 +0200

    feat: (Products/Services) First Draft

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import Hero from './Components/Hero'
 import AboutUs from './Components/AboutUs'
 import CTA from './Components/CTA'
+import Products_Services from './Components/Products_Services'
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <Hero />
       <CTA />
       <AboutUs />
-
+      <Products_Services />
     </>
   )
 }
---
+++ src/Components/Products_Services.jsx
@@ -0,0 +1,195 @@
+import React from "react";
+
+function Products_Services() {
+
+    return (
+
+        <section className="overflow-hidden bg-[#f4f2f2] py-8 sm:py-16">
+            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+
+                {/* Main-container */}
+                <div className="mx-auto grid max-w-2xl gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+
+                    <div className="lg:pr-8 lg:pt-4">
+                        <div className="lg:max-w-lg">
+
+                            <h1 className="mt-2 text-3xl font-bold tracking-tight text-neutral-800 sm:text-4xl">What we offer</h1>
+
+                            {/* First List */}
+                            <dl className="mt-5 max-w-xl space-y-8 text-base leading-7 text-neutral-600 lg:max-w-none">
+                                <div className="relative">
+                                    <ul className="space-y-2">
+                                        <li className="flex items-start gap-x-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 flex-shrink-0 text-[#3daf36] mt-1">
+                                                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" />
+                                            </svg>
+                                            <span className="font-medium text-neutral-800">
+                                                Building instruments that measure total alkali concentration
+                                            </span>
+                                        </li>
+                                        <li className="flex items-start gap-x-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 flex-shrink-0 text-[#3daf36] mt-1">
+                                                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" />
+                                            </svg>
+                                            <span className="font-medium text-neutral-800">
+                                                Building instruments that measure specific alkali molecules (Speciation)
+                                            </span>
+                                        </li>
+                                        <li className="flex items-start gap-x-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 flex-shrink-0 text-[#3daf36] mt-1">
+                                                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" />
+                                            </svg>
+                                            <span className="font-medium text-neutral-800">
+                                                Performing continuous measurements with high time resolution (1 second)
+                                            </span>
+                                        </li>
+                                        <li className="flex items-start gap-x-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 flex-shrink-0 text-[#3daf36] mt-1">
+                                                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" />
+                                            </svg>
+                                            <span className="font-medium text-neutral-800">
+                                                Measurements of alkali with high sensitivity (PPB Concentrations) which enables measurements on extremely diluted samples
+                                            </span>
+                                        </li>
+                                        <li className="flex items-start gap-x-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 flex-shrink-0 text-[#3daf36] mt-1">
+                                                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" />
+                                            </svg>
+                                            <span className="font-medium text-neutral-800">
+                                                Transfers of nanoparticles (Incl. nucleation, coagulation / agglomeration, evaporation, transport etc)
+                                            </span>
+                                        </li>
+                                        <li className="flex items-start gap-x-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 flex-shrink-0 text-[#3daf36] mt-1">
+                                                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" />
+                                            </svg>
+                                            <span className="font-medium text-neutral-800">
+                                                Measurements of alkali in hot or cold gas flows
+                                            </span>
+                                        </li>
+                                        <li className="flex items-start gap-x-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 flex-shrink-0 text-[#3daf36] mt-1">
+                                                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" />
+                                            </svg>
+                                            <span className="font-medium text-neutral-800">
+                                                Consulting service / Investigation / Analysis
+                                            </span>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </dl>
+                        </div>
+                    </div>
+
+                    <div className="lg:pr-8 lg:pt-4">
+                        
+                        <div className="lg:max-w-lg">
+
+                            <h1 className="mt-2 text-3xl font-bold tracking-tight text-neutral-800 sm:text-4xl">Our target groups</h1>
+
+                            {/* Second List */}
+                            <dl className="mt-5 max-w-xl space-y-8 text-base leading-7 text-neutral-600 lg:max-w-none">
+                                <div className="relative">
+                                    <ul className="space-y-2">
+                                        <li className="flex items-start gap-x-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 flex-shrink-0 text-sky-900 mt-1">
+                                                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" />
+                                            </svg>
+                                            <span className="font-medium text-neutral-800">
+                                                Marine aerosols
+                                            </span>
+                                        </li>
+                                        <li className="flex items-start gap-x-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 flex-shrink-0 text-sky-900 mt-1">
+                                                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" />
+                                            </svg>
+                                            <span className="font-medium text-neutral-800">
+                                                Climate monitoring
+                                            </span>
+                                        </li>
+                                        <li className="flex items-start gap-x-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 flex-shrink-0 text-sky-900 mt-1">
+                                                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" />
+                                            </svg>
+                                            <span className="font-medium text-neutral-800">
+                                                Fundamental research
+                                            </span>
+                                        </li>
+                                        <li className="flex items-start gap-x-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 flex-shrink-0 text-sky-900 mt-1">
+                                                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" />
+                                            </svg>
+                                            <span className="font-medium text-neutral-800">
+                                                Battery Sector (Li-Ion)
+                                            </span>
+                                        </li>
+                                        <li className="flex items-start gap-x-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 flex-shrink-0 text-sky-900 mt-1">
+                                                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" />
+                                            </svg>
+                                            <span className="font-medium text-neutral-800">
+                                                Catalysis Sector (Filter)
+                                            </span>
+                                        </li>
+                                        <li className="flex items-start gap-x-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 flex-shrink-0 text-sky-900 mt-1">
+                                                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" />
+                                            </svg>
+                                            <span className="font-medium text-neutral-800">
+                                                Carbon Capture and Storage
+                                            </span>
+                                        </li>
+                                        <li className="flex items-start gap-x-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 flex-shrink-0 text-sky-900 mt-1">
+                                                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" />
+                                            </svg>
+                                            <span className="font-medium text-neutral-800">
+                                                Measurement techniques / Sampling
+                                            </span>
+                                        </li>
+                                        <li className="flex items-start gap-x-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 flex-shrink-0 text-sky-900 mt-1">
+                                                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" />
+                                            </svg>
+                                            <span className="font-medium text-neutral-800">
+                                                Alkali chemistry and high temperature alkali
+                                            </span>
+                                        </li>
+                                        <li className="flex items-start gap-x-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 flex-shrink-0 text-sky-900 mt-1">
+                                                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" />
+                                            </svg>
+                                            <span className="font-medium text-neutral-800">
+                                                Energy Sector (Combustion, Gasification, Chemical looping combustion)
+                                            </span>
+                                        </li>
+                                        <li className="flex items-start gap-x-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 flex-shrink-0 text-sky-900 mt-1">
+                                                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" />
+                                            </svg>
+                                            <span className="font-medium text-neutral-800">
+                                                Air Quality Sector (Biomass burning emissions, Sea salt emissions)
+                                            </span>
+                                        </li>
+                                        <li className="flex items-start gap-x-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 flex-shrink-0 text-sky-900 mt-1">
+                                                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" />
+                                            </svg>
+                                            <span className="font-medium text-neutral-800">
+                                                Clean rooms / Sensitive environments (For example, measurements of the effect of cleaning filters etc)
+                                            </span>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </dl>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            
+        </section>
+    );
+}
+
+export default Products_Services;
