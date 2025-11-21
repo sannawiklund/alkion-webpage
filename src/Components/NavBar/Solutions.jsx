7313d7d362ddb58fcfc6c6a7593a1337bea7abc4
Sanna Wiklund <142911012+sannawiklund@users.noreply.github.com>
2025-11-21 23:12:03 +0100

    feat: (Services) Add Solutions-intro + Add new dark-accent colour

--- src/Components/NavBar/Solutions.jsx
+++ src/Components/NavBar/Solutions.jsx
@@ -26,36 +26,47 @@ function Solutions() {
   }, [hash]);
 
   return (
-    <div className="bg-accent-blue w-full">
+    <div className="bg-accent-dark-blue w-full">
 
-      <div style={{ width: '100%', height: '600px', position: 'relative' }}>
+      <div className="relative w-full h-[400px]">
+
+        {/* BACKGROUND EFFECT */}
         <Threads
-          amplitude={1}
-          distance={0}
-          enableMouseInteraction={true}
+          amplitude={1.5}
+          distance={0.55}
+          enableMouseInteraction={false}
+          color={[2, 5, 2]}
+          style={{
+            position: "absolute",
+            inset: 0,
+            width: "100%",
+            height: "100%",
+            zIndex: 0
+          }}
         />
+
+        {/* OVERLAY CONTENT */}
+        <div className="absolute inset-0 flex flex-col justify-center items-center text-center px-2 z-10">
+
+          <h1 className="text-4xl md:text-6xl font-semibold text-accent-green mb-2">
+            <TextType
+              text={["Our Solutions"]}
+              typingSpeed={75}
+              pauseDuration={1500}
+              showCursor={true}
+              cursorCharacter="|"
+              textColors={["var(--accent-green)"]}
+              className="inline"
+            />
+          </h1>
+
+          <p className="text-lg md:text-2xl text-accent-beige leading-relaxed max-w-4xl">
+            From extraction to conditioning and dilution - To data acquisition and analysis.
+          </p>
+
+        </div>
+
       </div>
-
-      {/* ---- SOLUTIONS INTRO ----- */}
-      <section id="solutions-intro" className="py-24 px-6 max-w-4xl mx-auto text-center">
-        <h1 className="text-4xl md:text-5xl font-semibold text-accent-green mb-6">
-          <TextType
-            text={["Our Solutions"]}
-            typingSpeed={75}
-            pauseDuration={1500}
-            showCursor={true}
-            cursorCharacter="|"
-            textColors={["var(--accent-green)"]}
-            className="inline"
-          />
-        </h1>
-
-        <p className="text-lg md:text-xl text-accent-beige leading-relaxed">
-          From extraction, to conditioning and
-          dilution, to data acquisition and analysis.
-        </p>
-
-      </section>
 
 
       {/* ---- SOLUTIONS PAGE-STRUCTURE -----*/}
